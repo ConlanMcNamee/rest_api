e8949f17a13a448c4c00bbdb3aad21d6b5b6e0b3
Conlan McNamee <conlanmcnamee@gmail.com>
2015-07-21 09:41:16 -0700

    added mongoose feature

--- movies/movies.js
+++ movies/movies.js
@@ -7,4 +7,8 @@ var moviesSchema = mongoose.Schema({
 	year: Number
 });
 
+moviesSchema.path("year").validate(function(v) {
+	return v > 1895;
+},"No film was made before 1986");
+
 module.exports = mongoose.model('Movies', moviesSchema);
